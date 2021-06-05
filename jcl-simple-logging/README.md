0443cef6a63be3007211f0944fd7101ee7feb987
Owen Carlsen <o@ocarlsen.com>
2021-06-05 16:51:14 -0700

    Adding jcl-simple-logging

---
+++ jcl-simple-logging/README.md
@@ -0,0 +1,3 @@
+# jcl-simple-logging
+
+Demo app showcasing JCL logging framework with SimpleLog.
---
+++ jcl-simple-logging/src/main/java/com/ocarlsen/app/App.java
@@ -0,0 +1,34 @@
+package com.ocarlsen.app;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Demo app showcasing JCL logging framework with SimpleLog.
+ */
+public class App {
+
+    private final Log log;
+
+    public App() {
+        log = LogFactory.getLog(getClass());
+    }
+
+    private void demoLogger() {
+
+        log.fatal("fatal");
+        log.error("error");
+        log.warn("warn");
+        log.info("info");
+        log.debug("debug");
+        log.trace("trace");
+
+        // Log an exception.
+        RuntimeException ex = new RuntimeException("Oops!");
+        log.warn(ex.getMessage(), ex);
+    }
+
+    public static void main(String[] args) {
+        new App().demoLogger();
+    }
+}
